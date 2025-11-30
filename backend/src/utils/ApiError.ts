56178fa25f9fa661f866e262788811431e69f318
Akash4701 <saha.akash4701@gmail.com>
2025-12-01 03:18:50 +0530

    added error handling

---
+++ backend/src/utils/ApiError.ts
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+   
+    public statusCode: number;
+    public success: boolean;
+    public errors: string[];
+    public data: null;
+
+    constructor(statusCode: number, message: string, errors: string[] = [], stack?: string) {
+        super(message);
+        this.statusCode = statusCode;
+
+        this.message = message;
+        this.success = false;
+        this.data=null;
+
+        this.errors = errors;
+        if (stack) {
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
